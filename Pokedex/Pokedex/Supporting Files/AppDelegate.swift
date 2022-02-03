41cbd64748ee3765f8ebd6a7793388702df44cf7
Karl Pfister <kpfister@me.com>
2022-02-03 11:15:06 -0700

    Resets app to Starter

--- Pokedex/Pokedex/Supporting Files/AppDelegate.swift
+++ Pokedex/Pokedex/Supporting Files/AppDelegate.swift
@@ -14,11 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        NetworkingController.fetchPokemon(with: "") { ditto in
-            if let ditto = ditto {
-                print(ditto.name)
-            }
-        }
         return true
     }
 
