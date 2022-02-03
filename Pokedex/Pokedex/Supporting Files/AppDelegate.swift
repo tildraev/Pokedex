ed55603824e47c2166c8c225791c80a2b1d1ff41
Karl Pfister <kpfister@me.com>
2022-02-03 11:13:03 -0700

    Completes Master Project

--- Pokedex/Pokedex/Supporting Files/AppDelegate.swift
+++ Pokedex/Pokedex/Supporting Files/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        NetworkingController.fetchPokemon(with: "") { ditto in
+            if let ditto = ditto {
+                print(ditto.name)
+            }
+        }
         return true
     }
 
